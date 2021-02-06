8e244949300da691f57ba29da94e044e560877c7
Aliaksandr Kuzaukou <Aliaksandr_Kuzaukou@epam.com>
2021-02-06 11:37:19 +0300

    add total sum and total quantity to cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -3,6 +3,8 @@ import { ProductModel } from '../models/product-model';
 import { ProductService } from '../services/product.service';
 @Injectable()
 export class CartService {
+  public totalSum = 0;
+  public totalQuantity = 0;
   shoppingList: ProductModel[] = [];
   constructor(private productsService: ProductService) { }
 
@@ -12,17 +14,30 @@ export class CartService {
       product.quantity = 1;
       this.shoppingList.push(this.productsService.getProductById(id));
       this.productsService.updateProductByIdIsAvailableValue(id, false);
+      this.updateCartTotalSumAndQuantity();
     } else {
       this.shoppingList.forEach(item => {
         if (item.id === id) {
           item.quantity += 1;
         }
+        this.updateCartTotalSumAndQuantity();
       });
     }
   }
 
   removeProductFromCart(id: number): void {
     this.shoppingList = this.shoppingList.filter(item => item.id !== id);
+    this.updateCartTotalSumAndQuantity();
+  }
+
+  updateCartTotalSumAndQuantity(): void{
+    this.totalQuantity = this.shoppingList.reduce((previousValue, currentValue) => {
+      return previousValue + currentValue.quantity
+    }, 0);
+
+    this.totalSum = this.shoppingList.reduce((previousValue, currentValue) => {
+      return previousValue + currentValue.price
+    }, 0);
   }
 
   private isProductInCart(id: number): boolean {
