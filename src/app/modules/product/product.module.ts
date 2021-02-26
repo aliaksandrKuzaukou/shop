484eca217a0eb8b09ef1dd3d04c1cb634f180bfa
Aliaksandr Kuzaukou <Aliaksandr_Kuzaukou@epam.com>
2021-02-26 12:19:20 +0300

    Task 2 changes in html and css

--- src/app/modules/product/product.module.ts
+++ src/app/modules/product/product.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductItemComponent } from '../product/product-item/product-item.component';
 import { ProductListComponent } from '../product/product-list/product-list.component';
-import { FirstComponentComponent } from '../product/first-component/first-component.component';
 import { SharedModule } from './../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    FirstComponentComponent,
     ProductItemComponent,
     ProductListComponent
   ],
@@ -17,7 +15,6 @@ import { SharedModule } from './../shared/shared.module';
     SharedModule
   ],
   exports: [
-    FirstComponentComponent,
     ProductItemComponent,
     ProductListComponent
   ]
